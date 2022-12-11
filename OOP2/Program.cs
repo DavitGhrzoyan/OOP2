d8577c190c0c76140ed0482b949e98dec3361d15
Davit <ghrzoyandavit@gmail.com>
2022-12-11 22:30:48 +0400

    Add project files.

---
+++ OOP2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace OOP2
+{
+
+    class Program
+    {
+        static Student GetStudent()
+        {
+            Student student = new Student();
+
+            student.firstName = "Davit";
+            student.middleName = "Matevos";
+            student.lastName = "Ghrzoyan";
+            student.age = 27;
+            student.id = Guid.NewGuid();
+            student.group = "C-Sharp 2022";
+
+            return student;
+        }
+
+        //static Student GetStudent()
+        //{
+        //    Student secondStudent = new Student();
+
+        //    secondStudent.firstName = "Armen";
+        //    secondStudent.middleName = "Hayk";
+        //    secondStudent.lastName = "Martirosyan";
+        //    secondStudent.age = 22;
+        //    secondStudent.id = Guid.NewGuid();
+        //    secondStudent.group = "Java Script 2022";
+        //    secondStudent.Print();
+
+        //    return secondStudent;
+        //}
+
+        //static void Print(Student student)
+        //{
+        //    Console.WriteLine("Information about student");
+        //    Console.WriteLine($"Id: {student.id}");
+        //    Console.WriteLine($"Last Name: {student.lastName}");
+        //    Console.WriteLine($"Name: {student.firstName}");
+        //    Console.WriteLine($"Surname: { student.middleName}");
+        //    Console.WriteLine($"Age: {student.age}");
+        //    Console.WriteLine($"Group: {student.group}");
+        //}
+
+        static void Main(string[] args)
+        {
+            var firstStudent = GetStudent();
+
+            string fullName = firstStudent.GetFullName();
+
+            Console.WriteLine(fullName);
+
+
+        }
+    }
+} 
---
+++ OOP2/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Student
+    {
+        public Guid id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public string group;
+
+        public void Print()
+        {
+            Console.WriteLine("Information about student");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Last Name: {lastName}");
+            Console.WriteLine($"Name: {firstName}");
+            Console.WriteLine($"Surname: {middleName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Group: {group}");
+        }
+
+        public string GetFullName()
+        {
+            return $"{lastName } {firstName} {middleName}";
+        }
+    }
+}
